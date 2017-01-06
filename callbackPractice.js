7b13c3dbc8d53e7d95a56df3dce725f7661e039c
raleigh wayland <rtwayland@gmail.com>
2017-01-06 14:43:55 -0700

    Finish problems

--- callbackPractice.js
+++ callbackPractice.js
@@ -123,7 +123,11 @@ uniq(names, function(uniqArr) {
 
 
 
-//Code Here for each
+var each = function(names, cb) {
+    for (var i = 0; i < names.length; i++) {
+        cb(names[i], i);
+    }
+}
 
 each(names, function(item, indice) {
     console.log('The item in the ' + indice + ' position is ' + item)
@@ -139,7 +143,14 @@ each(names, function(item, indice) {
 
 
 
-//code here for getUserById
+var getUserById = function(users, id, cb) {
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].id === id) {
+            cb(users[i]);
+            break;
+        }
+    }
+};
 
 var users = [{
         id: '12d',
